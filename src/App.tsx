2146b2a28da3b5c2a7011a234dbbbe050aa1d432
jawad.almoosawi <jawad.almoosawi@innovateq.digital>
2025-06-08 19:31:43 +0400

    [fixed] text is duplicated when the shape is duplicated

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { useEffect, useState } from "react";
-import { createShapeId, Editor, intersectLineSegmentPolygon, stopEventPropagation, TLArrowBinding, TLArrowShape, 
-  TLComponents, Tldraw, TLShapeId, useEditor, useValue, Vec,hardReset
-} from 'tldraw'
+import { Editor, Tldraw, hardReset } from 'tldraw'
 import 'tldraw/tldraw.css'
 import { save,open,ask } from '@tauri-apps/plugin-dialog';
 import { writeTextFile, readTextFile } from '@tauri-apps/plugin-fs';
 import { Menu, MenuItem, Submenu } from '@tauri-apps/api/menu';
-import { Button, message,Popover } from 'antd';
+import { message } from 'antd';
 import { invoke } from '@tauri-apps/api/core';
 import { getCurrentWindow  } from "@tauri-apps/api/window";
-
+import { shapeButtons } from "./components/tldraw/shapeButtons";
 
 getCurrentWindow().listen("my-window-event", ({ event, payload }) => {
   console.log(event)
@@ -29,15 +27,6 @@ function App() {
       content: 'Saved successfully',
     });
   };
-
-  // const error = () => {
-  //   messageApi.open({
-  //     type: 'error',
-  //     content: 'This is an error message',
-  //   });
-  // };
-  
-
 
 
   useEffect(() => {
@@ -232,284 +221,7 @@ function App() {
     }
   }
 
-  const components: TLComponents = {
-    InFrontOfTheCanvas: () => {
-      
-      const editor = useEditor()
-      //console.log(editor.getOnlySelectedShape())
-      if(!editor)
-          return;
-      if(editor.getOnlySelectedShape()?.type !="geo")
-          return;
-      const info = useValue(
-        'selection bounds',
-        () => {
-          const screenBounds = editor.getViewportScreenBounds()
-          const rotation = editor.getSelectionRotation()
-          const rotatedScreenBounds = editor.getSelectionRotatedScreenBounds()
-          if (!rotatedScreenBounds) return
-          return {
-            // we really want the position within the
-            // tldraw component's bounds, not the screen itself
-            x: rotatedScreenBounds.x - screenBounds.x,
-            y: rotatedScreenBounds.y - screenBounds.y,
-            width: rotatedScreenBounds.width,
-            height: rotatedScreenBounds.height,
-            rotation: rotation,
-          }
-        },
-        [editor]
-      )
   
-      if (!info) return
-  
-      return (
-        <div
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            transformOrigin: 'top left',
-            transform: `translate(${info.x}px, ${info.y}px) rotate(${info.rotation}rad)`,
-            pointerEvents: 'all',
-          }}
-          onPointerDown={stopEventPropagation}
-        >
-          <DuplicateInDirectionButton y={-40} x={info.width / 2 - 16} rotation={-(Math.PI / 2)} />
-          <DuplicateInDirectionButton y={info.height / 2 - 16} x={info.width + 8} rotation={0} />
-          <DuplicateInDirectionButton y={info.height + 8} x={info.width / 2 - 16} rotation={(Math.PI / 2)} />
-          <DuplicateInDirectionButton y={info.height / 2 - 16} x={-40} rotation={Math.PI} />
-        </div>
-      )
-    },
-  }
-
-
-  const content = (
-    <div style={{ marginTop: '-25px' }}>
-      <p>Draw line from shape</p>
-    </div>
-    
-);
-  
-  function DuplicateInDirectionButton({
-    x,
-    y,
-    rotation,
-  }: {
-    x: number
-    y: number
-    rotation: number
-  }) {
-    const editor = useEditor()
-    return (
-<Popover content={content}  styles={{ body: {height: '5px'} }}>
-<Button
-shape="circle"
-style={{
-  position: 'absolute',
-  width: '32px',
-  height: '32px',
-  pointerEvents: 'all',
-  transform: `translate(${x}px, ${y}px) rotate(${rotation}rad)`,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  padding: 0
-}}
-onPointerDown={stopEventPropagation}
-onClick={() => {
-  DuplicateShape(rotation, editor);
-}}
->
-<span style={{ 
-        fontWeight: 800, 
-        fontSize: '16px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: '100%',
-        lineHeight: 1,
-        marginTop: '-2px'
-      }}>→</span>
-</Button>
-  </Popover>
-  
-      // <button
-      // 	style={{
-      // 		position: 'absolute',
-      // 		width: 32,
-      // 		height: 32,
-      // 		pointerEvents: 'all',
-      // 		transform: `translate(${x}px, ${y}px) rotate(${rotation}rad)`,
-      // 	}}
-      // 	onPointerDown={stopEventPropagation}
-      // 	onClick={() => {
-      // 		DuplicateShape(rotation)
-      // 	}}
-      // >
-      // 	→
-      // </button>
-    )
-  }
-  
-  
-  const DuplicateShape = (rotation: number,editor : Editor) => {
-    
-    const selectionRotation = editor.getSelectionRotation() ?? 0
-    const rotatedPageBounds = editor.getSelectionRotatedPageBounds()!
-    const selectionPageBounds = editor.getSelectionPageBounds()!
-    if (!(rotatedPageBounds && selectionPageBounds)) return
-
-    editor.markHistoryStoppingPoint()
-
-    const PADDING = 32
-
-    // Find an intersection with the page bounds
-    const center = Vec.Rot(rotatedPageBounds.center, selectionRotation)
-    const int = intersectLineSegmentPolygon(
-      center,
-      Vec.Add(center, new Vec(100000, 0).rot(selectionRotation + rotation)),
-      rotatedPageBounds
-        .clone()
-        .expandBy(PADDING)
-        .corners.map((c) => c.rot(selectionRotation))
-    )
-    if (!int?.[0]) return
-
-    // Get the direction and distance to the intersection
-    const delta = Vec.Sub(int[0], center)
-    const dist = delta.len()
-    const dir = delta.norm()
-
-    // Get the offset for the duplicated shapes
-    const offset = dir.mul(dist * 2)
-
-
-
-    const originalShapes = editor.getSelectedShapeIds()
-    console.log(originalShapes)
-    //const originalShapeIds = Array.from(originalShapes)
-    const duplicatedShapesMap = editor.duplicateShapes(editor.getSelectedShapes(), offset)
-    console.log(duplicatedShapesMap)
-
-
-    const newlySelectedShapes = editor.getSelectedShapeIds();
-
-
-
-    createArrowBetweenShapes(editor,originalShapes[0],newlySelectedShapes[0]);
-  }
-  
-  function createArrowBetweenShapes(
-    editor: Editor,
-    startShapeId: TLShapeId,
-    endShapeId: TLShapeId,
-    options = {} as {
-      parentId?: TLShapeId
-      start?: Partial<Omit<TLArrowBinding['props'], 'terminal'>>
-      end?: Partial<Omit<TLArrowBinding['props'], 'terminal'>>
-    }
-  ) {
-    const { start = {}, end = {}, parentId } = options
-  
-    // [1]
-    const {
-      normalizedAnchor: startNormalizedAnchor = { x: 0.5, y: 0.5 },
-      isExact: startIsExact = false,
-      isPrecise: startIsPrecise = false,
-    } = start
-    const {
-      normalizedAnchor: endNormalizedAnchor = { x: 0.5, y: 0.5 },
-      isExact: endIsExact = false,
-      isPrecise: endIsPrecise = false,
-    } = end
-  
-    const startTerminalNormalizedPosition = Vec.From(startNormalizedAnchor)
-    const endTerminalNormalizedPosition = Vec.From(endNormalizedAnchor)
-  
-    const parent = parentId ? editor.getShape(parentId) : undefined
-    if (parentId && !parent) throw Error(`Parent shape with id ${parentId} not found`)
-  
-    const startShapePageBounds = editor.getShapePageBounds(startShapeId)
-    const endShapePageBounds = editor.getShapePageBounds(endShapeId)
-  
-    const startShapePageRotation = editor.getShapePageTransform(startShapeId).rotation()
-    const endShapePageRotation = editor.getShapePageTransform(endShapeId).rotation()
-  
-    if (!startShapePageBounds || !endShapePageBounds) return
-  
-    const startTerminalPagePosition = Vec.Add(
-      startShapePageBounds.point,
-      Vec.MulV(
-        startShapePageBounds.size,
-        Vec.Rot(startTerminalNormalizedPosition, startShapePageRotation)
-      )
-    )
-    const endTerminalPagePosition = Vec.Add(
-      endShapePageBounds.point,
-      Vec.MulV(
-        startShapePageBounds.size,
-        Vec.Rot(endTerminalNormalizedPosition, endShapePageRotation)
-      )
-    )
-  
-    const arrowPointInParentSpace = Vec.Min(startTerminalPagePosition, endTerminalPagePosition)
-    if (parent) {
-      arrowPointInParentSpace.setTo(
-        editor.getShapePageTransform(parent.id)!.applyToPoint(arrowPointInParentSpace)
-      )
-    }
-  
-    const arrowId = createShapeId()
-    editor.run(() => {
-      editor.markHistoryStoppingPoint('creating_arrow')
-      editor.createShape<TLArrowShape>({
-        id: arrowId,
-        type: 'arrow',
-        // [2]
-        x: arrowPointInParentSpace.x,
-        y: arrowPointInParentSpace.y,
-        props: {
-          // [3]
-          start: {
-            x: arrowPointInParentSpace.x - startTerminalPagePosition.x,
-            y: arrowPointInParentSpace.x - startTerminalPagePosition.x,
-          },
-          end: {
-            x: arrowPointInParentSpace.x - endTerminalPagePosition.x,
-            y: arrowPointInParentSpace.x - endTerminalPagePosition.x,
-          },
-        },
-      })
-  
-      editor.createBindings<TLArrowBinding>([
-        {
-          fromId: arrowId,
-          toId: startShapeId,
-          type: 'arrow',
-          props: {
-            terminal: 'start',
-            normalizedAnchor: startNormalizedAnchor,
-            isExact: startIsExact,
-            isPrecise: startIsPrecise,
-          },
-        },
-        {
-          fromId: arrowId,
-          toId: endShapeId,
-          type: 'arrow',
-          props: {
-            terminal: 'end',
-            normalizedAnchor: endNormalizedAnchor,
-            isExact: endIsExact,
-            isPrecise: endIsPrecise,
-          },
-        },
-      ])
-    })
-  }
 
   return (
     <main className="container">
@@ -525,7 +237,7 @@ onClick={() => {
             console.log("new editor set")
           }
           editor.user.updateUserPreferences({ isSnapMode: true })
-          }} components={components} />
+          }} components={shapeButtons} />
       </div>
     </div>
   
---
+++ src/components/tldraw/shapeButtons.tsx
@@ -0,0 +1,271 @@
+import { createShapeId, Editor, intersectLineSegmentPolygon, stopEventPropagation, TLArrowBinding, TLArrowShape, 
+  TLComponents, TLShapeId, useEditor, useValue, Vec
+} from 'tldraw'
+import 'tldraw/tldraw.css'
+import { Button, Popover } from 'antd';
+
+
+export const shapeButtons: TLComponents = {
+    InFrontOfTheCanvas: () => {
+      
+      const editor = useEditor()
+      //console.log(editor.getOnlySelectedShape())
+      if(!editor)
+          return;
+      if(editor.getOnlySelectedShape()?.type !="geo")
+          return;
+      const info = useValue(
+        'selection bounds',
+        () => {
+          const screenBounds = editor.getViewportScreenBounds()
+          const rotation = editor.getSelectionRotation()
+          const rotatedScreenBounds = editor.getSelectionRotatedScreenBounds()
+          if (!rotatedScreenBounds) return
+          return {
+            // we really want the position within the
+            // tldraw component's bounds, not the screen itself
+            x: rotatedScreenBounds.x - screenBounds.x,
+            y: rotatedScreenBounds.y - screenBounds.y,
+            width: rotatedScreenBounds.width,
+            height: rotatedScreenBounds.height,
+            rotation: rotation,
+          }
+        },
+        [editor]
+      )
+  
+      if (!info) return
+  
+      return (
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            transformOrigin: 'top left',
+            transform: `translate(${info.x}px, ${info.y}px) rotate(${info.rotation}rad)`,
+            pointerEvents: 'all',
+          }}
+          onPointerDown={stopEventPropagation}
+        >
+          <DuplicateInDirectionButton y={-40} x={info.width / 2 - 16} rotation={-(Math.PI / 2)} />
+          <DuplicateInDirectionButton y={info.height / 2 - 16} x={info.width + 8} rotation={0} />
+          <DuplicateInDirectionButton y={info.height + 8} x={info.width / 2 - 16} rotation={(Math.PI / 2)} />
+          <DuplicateInDirectionButton y={info.height / 2 - 16} x={-40} rotation={Math.PI} />
+        </div>
+      )
+    },
+}
+
+
+const content = (
+  <div style={{ marginTop: '-25px' }}>
+    <p>Draw line from shape</p>
+  </div>
+);
+  
+function DuplicateInDirectionButton({
+    x,
+    y,
+    rotation,
+  }: {
+    x: number
+    y: number
+    rotation: number
+  }) {
+    const editor = useEditor()
+    return (
+<Popover content={content}  styles={{ body: {height: '5px'} }}>
+<Button
+shape="circle"
+style={{
+  position: 'absolute',
+  width: '32px',
+  height: '32px',
+  pointerEvents: 'all',
+  transform: `translate(${x}px, ${y}px) rotate(${rotation}rad)`,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: 0
+}}
+onPointerDown={stopEventPropagation}
+onClick={() => {
+  DuplicateShape(rotation, editor);
+}}
+>
+<span style={{ 
+        fontWeight: 800, 
+        fontSize: '16px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+        lineHeight: 1,
+        marginTop: '-2px'
+      }}>→</span>
+</Button>
+  </Popover>
+    )
+}
+  
+  
+const DuplicateShape = (rotation: number,editor : Editor) => {
+    
+    const selectionRotation = editor.getSelectionRotation() ?? 0
+    const rotatedPageBounds = editor.getSelectionRotatedPageBounds()!
+    const selectionPageBounds = editor.getSelectionPageBounds()!
+    if (!(rotatedPageBounds && selectionPageBounds)) return
+
+    editor.markHistoryStoppingPoint()
+
+    const PADDING = 32
+
+    // Find an intersection with the page bounds
+    const center = Vec.Rot(rotatedPageBounds.center, selectionRotation)
+    const int = intersectLineSegmentPolygon(
+      center,
+      Vec.Add(center, new Vec(100000, 0).rot(selectionRotation + rotation)),
+      rotatedPageBounds
+        .clone()
+        .expandBy(PADDING)
+        .corners.map((c) => c.rot(selectionRotation))
+    )
+    if (!int?.[0]) return
+
+    // Get the direction and distance to the intersection
+    const delta = Vec.Sub(int[0], center)
+    const dist = delta.len()
+    const dir = delta.norm()
+
+    // Get the offset for the duplicated shapes
+    const offset = dir.mul(dist * 2)
+
+
+
+    const originalShapes = editor.getSelectedShapeIds()
+    console.log(originalShapes)
+    console.log(editor.getSelectedShapes())
+
+    const newShape = editor.getSelectedShapes();
+    const duplicatedShapesMap = editor.duplicateShapes(newShape, offset)
+    console.log(duplicatedShapesMap)
+
+
+    const newlySelectedShapes = editor.getSelectedShapeIds();
+    editor.updateShape({ id: newlySelectedShapes[0], type: newShape[0].type, props: { richText : {
+      type: "doc",
+      content: [{ type: "paragraph" }]
+    }}})
+
+    createArrowBetweenShapes(editor,originalShapes[0],newlySelectedShapes[0]);
+}
+  
+function createArrowBetweenShapes(
+    editor: Editor,
+    startShapeId: TLShapeId,
+    endShapeId: TLShapeId,
+    options = {} as {
+      parentId?: TLShapeId
+      start?: Partial<Omit<TLArrowBinding['props'], 'terminal'>>
+      end?: Partial<Omit<TLArrowBinding['props'], 'terminal'>>
+    }
+  ) {
+    const { start = {}, end = {}, parentId } = options
+  
+    // [1]
+    const {
+      normalizedAnchor: startNormalizedAnchor = { x: 0.5, y: 0.5 },
+      isExact: startIsExact = false,
+      isPrecise: startIsPrecise = false,
+    } = start
+    const {
+      normalizedAnchor: endNormalizedAnchor = { x: 0.5, y: 0.5 },
+      isExact: endIsExact = false,
+      isPrecise: endIsPrecise = false,
+    } = end
+  
+    const startTerminalNormalizedPosition = Vec.From(startNormalizedAnchor)
+    const endTerminalNormalizedPosition = Vec.From(endNormalizedAnchor)
+  
+    const parent = parentId ? editor.getShape(parentId) : undefined
+    if (parentId && !parent) throw Error(`Parent shape with id ${parentId} not found`)
+  
+    const startShapePageBounds = editor.getShapePageBounds(startShapeId)
+    const endShapePageBounds = editor.getShapePageBounds(endShapeId)
+  
+    const startShapePageRotation = editor.getShapePageTransform(startShapeId).rotation()
+    const endShapePageRotation = editor.getShapePageTransform(endShapeId).rotation()
+  
+    if (!startShapePageBounds || !endShapePageBounds) return
+  
+    const startTerminalPagePosition = Vec.Add(
+      startShapePageBounds.point,
+      Vec.MulV(
+        startShapePageBounds.size,
+        Vec.Rot(startTerminalNormalizedPosition, startShapePageRotation)
+      )
+    )
+    const endTerminalPagePosition = Vec.Add(
+      endShapePageBounds.point,
+      Vec.MulV(
+        startShapePageBounds.size,
+        Vec.Rot(endTerminalNormalizedPosition, endShapePageRotation)
+      )
+    )
+  
+    const arrowPointInParentSpace = Vec.Min(startTerminalPagePosition, endTerminalPagePosition)
+    if (parent) {
+      arrowPointInParentSpace.setTo(
+        editor.getShapePageTransform(parent.id)!.applyToPoint(arrowPointInParentSpace)
+      )
+    }
+  
+    const arrowId = createShapeId()
+    editor.run(() => {
+      editor.markHistoryStoppingPoint('creating_arrow')
+      editor.createShape<TLArrowShape>({
+        id: arrowId,
+        type: 'arrow',
+        x: arrowPointInParentSpace.x,
+        y: arrowPointInParentSpace.y,
+        props: {
+          kind: 'elbow',
+          start: {
+            x: arrowPointInParentSpace.x - startTerminalPagePosition.x,
+            y: arrowPointInParentSpace.x - startTerminalPagePosition.x,
+          },
+          end: {
+            x: arrowPointInParentSpace.x - endTerminalPagePosition.x,
+            y: arrowPointInParentSpace.x - endTerminalPagePosition.x,
+          },
+        },
+      })
+  
+      editor.createBindings<TLArrowBinding>([
+        {
+          fromId: arrowId,
+          toId: startShapeId,
+          type: 'arrow',
+          props: {
+            terminal: 'start',
+            normalizedAnchor: startNormalizedAnchor,
+            isExact: startIsExact,
+            isPrecise: startIsPrecise,
+          },
+        },
+        {
+          fromId: arrowId,
+          toId: endShapeId,
+          type: 'arrow',
+          props: {
+            terminal: 'end',
+            normalizedAnchor: endNormalizedAnchor,
+            isExact: endIsExact,
+            isPrecise: endIsPrecise,
+          },
+        },
+      ])
+    })
+}
